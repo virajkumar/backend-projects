87969045ea82f7519c32046f6bd84c07f4d537ed
virajkumar <virajkumar12345@gmail.com>
2024-10-19 17:37:30 +0530

    almost done with ch5

--- backend/src/db/init.js
+++ backend/src/db/init.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 
 export function initDatabase() {
-  const DATABASE_URL = process.env.DATABASE_URL
+  const DATABASE_URL = process.env.DATABASE_URL.toString();
+  console.log(typeof (DATABASE_URL));
   mongoose.connection.on('open', () => {
     console.info('successfully connected to database:', DATABASE_URL)
   })
